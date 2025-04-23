47e65037495c94f87c49a9be7d62f80d671d0c6e
Facuindo <167240285+Facuindo@users.noreply.github.com>
2025-04-23 15:39:04 -0300

    Error

---
+++ Assets/Nolose.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Nolose : MonoBehaviour
+{
+    string clave = "tic2025";
+   
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ComprobarClaveIngresada()
+    {
+        string claveIngresada = inputClave.text;
+        if (claveIngresada != clave)
+        {
+            txtMensaje.text = "Acceso denegado";
+        }
+        else
+        {
+            txtMensaje.text = "Bienvenido";
+        }
+    }
+}
